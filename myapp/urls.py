a8f0075e074f80fddf78bc827bcb8e428985d202
Sanjay Marathi <sanjaymarathiiii@gmail.com>
2025-10-15 18:25:39 +0530

    complete menu

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,14 @@ from . import views
 app_name = 'myapp'
 
 urlpatterns = [
-    path('', views.index),
-    path('item/',views.item),
-    path('<int:id>/',views.detail, name='detail'),
+    # Read/List View
+    path('', views.index, name='index'), 
+    # Detail View
+    path('<int:id>/', views.detail, name='detail'), 
+    # Create View
+    path('add/', views.create_item, name='create_item'),
+    # Update View
+    path('update/<int:id>/', views.update_item, name='item_update'),
+    # NEW: Delete View
+    path('delete/<int:id>/', views.delete_item, name='item_delete'), 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Item
-# Create your views here.
+from .forms import ItemForm
+
+# Menu List View (Index)
 def index(request):
     item_list = Item.objects.all()
     context = {
@@ -9,12 +11,51 @@ def index(request):
     }
     return render(request,"myapp/index.html",context)
 
+# Item Detail View
 def detail(request, id):
     item = Item.objects.get(id = id)
     return render(request, 'myapp/detail.html', {
         'item':item
     })
       
-    
-def item(reques):
-    return HttpResponse("<h1>This is an item view.</h1>")
+# Create Item View
+def create_item(request):
+    form = ItemForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('myapp:index')
+        
+    context = {
+        'form':form,
+        'title': 'Add New Food Item' # Context for form title
+    }
+    return render(request, 'myapp/item_form.html', context)
+
+# Update Item View
+def update_item(request, id):
+    item = Item.objects.get(id = id)
+    form = ItemForm(request.POST or None, instance = item)
+    if request.method == 'POST': 
+        if form.is_valid():
+            form.save()
+            return redirect('myapp:detail', id=item.id) # Redirect to detail after update
+            
+    context = {
+        'form':form,
+        'item': item, # Pass item instance for potential use in template
+        'title': f'Update {item.item_name}' # Context for form title
+    }
+    return render(request, 'myapp/item_form.html', context)
+
+# NEW: Delete Item View
+def delete_item(request, id): 
+    item = Item.objects.get(id = id)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('myapp:index') # Redirect to menu list after deletion
+        
+    context = {
+        'item': item
+    }
+    return render(request, 'myapp/item_delete.html', context)
