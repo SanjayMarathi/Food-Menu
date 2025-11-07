e1db3a4561f9dc7a23df6330429db075281e1a58
Sanjay Marathi <sanjaymarathiiii@gmail.com>
2025-11-07 15:16:20 +0530

    read guide fixed

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,9 +9,11 @@ from django.urls import path
 from django.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
     path('users/', include('users.urls')),
+    path("how-to-use/", TemplateView.as_view(template_name="myapp/how_to_use.html"), name="how_to_use"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
